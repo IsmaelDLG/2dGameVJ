18b5e29ff45f9e4412b0b19b6ea1856f903f5456
EnricSegura <enricsegurasanchez@gmail.com>
2019-10-23 11:11:00 +0200

    SpriteSheet contra

--- 2DGame/03-Contra/03-Contra/Player.cpp
+++ 2DGame/03-Contra/03-Contra/Player.cpp
@@ -13,32 +13,39 @@
 
 enum PlayerAnims
 {
-	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT
+	STAND_LEFT, STAND_RIGHT, MOVE_LEFT, MOVE_RIGHT, JUMPING
 };
 
 
 void Player::init(const glm::ivec2 &tileMapPos, ShaderProgram &shaderProgram)
 {
 	bJumping = false;
-	spritesheet.loadFromFile("images/retro-character.png", TEXTURE_PIXEL_FORMAT_RGBA);
-	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0.25, 0.25), &spritesheet, &shaderProgram);
-	sprite->setNumberAnimations(4);
+	spritesheet.loadFromFile("images/Contra_PC_Spritesheet.png", TEXTURE_PIXEL_FORMAT_RGBA);
+	sprite = Sprite::createSprite(glm::ivec2(32, 32), glm::vec2(0.125, 0.125), &spritesheet, &shaderProgram);
+	sprite->setNumberAnimations(5);
 	
 		sprite->setAnimationSpeed(STAND_LEFT, 8);
-		sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.f));
+		sprite->addKeyframe(STAND_LEFT, glm::vec2(0.f, 0.125f));
 		
 		sprite->setAnimationSpeed(STAND_RIGHT, 8);
-		sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.25f, 0.f));
+		sprite->addKeyframe(STAND_RIGHT, glm::vec2(0.f, 0.125f));
 		
 		sprite->setAnimationSpeed(MOVE_LEFT, 8);
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.25f));
-		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.f, 0.5f));
+		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.0f, 0.250f));
+		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.125f, 0.250f));
+		sprite->addKeyframe(MOVE_LEFT, glm::vec2(0.250f, 0.250f));
 		
 		sprite->setAnimationSpeed(MOVE_RIGHT, 8);
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.25f));
-		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.25, 0.5f));
+		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.0, 0.250f));
+		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.125f, 0.250f));
+		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.250f, 0.250f));
+		sprite->addKeyframe(MOVE_RIGHT, glm::vec2(0.375f, 0.250f));
+
+		sprite->setAnimationSpeed(JUMPING, 8);
+		sprite->addKeyframe(JUMPING, glm::vec2(0.500f, 0.f));
+		sprite->addKeyframe(JUMPING, glm::vec2(0.625f, 0.f));
+		sprite->addKeyframe(JUMPING, glm::vec2(0.750f, 0.f));
+		sprite->addKeyframe(JUMPING, glm::vec2(0.875f, 0.f));
 		
 	sprite->changeAnimation(0);
 	tileMapDispl = tileMapPos;
@@ -81,6 +88,7 @@ void Player::update(int deltaTime)
 	
 	if(bJumping)
 	{
+		sprite->changeAnimation(JUMPING);
 		jumpAngle += JUMP_ANGLE_STEP;
 		if(jumpAngle == 180)
 		{
