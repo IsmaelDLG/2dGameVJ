13516115181f04c266c6b1eaf3f5879535cc972b
EnricSegura <enricsegurasanchez@gmail.com>
2019-10-25 14:25:38 +0200

    Merge animations

--- 2DGame/03-Contra/03-Contra/Scene.cpp
+++ 2DGame/03-Contra/03-Contra/Scene.cpp
@@ -73,15 +73,6 @@ void Scene::update(int deltaTime)
 	if ((player->getPlayerPos().x - playerPos.x != 0) || (player->getPlayerPos().y - playerPos.y != 0)) {
 		playerPos = player->getPlayerPos();
 		projection = glm::ortho(0.f, float(CAMERA_WIDTH - 1), float(CAMERA_HEIGHT - 1), 0.f);
-		cameraX = (playerPos.x) - (CAMERA_WIDTH / 2);
-		//cameraY = (playerPos.y) - (CAMERA_HEIGHT / 2);
-		if (cameraX > offsetMaxX) cameraX = offsetMaxX;
-		else if (cameraX < offsetMinX) cameraX = offsetMinX;
-		//if (cameraY > offsetMaxY) cameraY = offsetMaxY;
-		//else if (cameraY < offsetMinY) cameraY = offsetMinY;
-		cameraY = 0.f;
-
-=======
 		cameraX = (playerPos.x) - (CAMERA_WIDTH / 3);
 		cameraY = (playerPos.y) - (CAMERA_HEIGHT / 2);
 		if (cameraX > offsetMaxX) cameraX = offsetMaxX;
@@ -89,19 +80,12 @@ void Scene::update(int deltaTime)
 		if (cameraY > offsetMaxY) cameraY = offsetMaxY;
 		else if (cameraY < offsetMinY) cameraY = offsetMinY;
 		offsetMinX = cameraX;
->>>>>>> SideScrollerMap
 	}
 	else {
 		cameraX = 0.f;
 		cameraY = 0.f;
 	}
-
-<<<<<<< HEAD
-	projection = glm::translate(projection, glm::vec3(-cameraX, 0, 0.f));
-=======
 	projection = glm::translate(projection, glm::vec3(-cameraX, -cameraY, 0.f));
-	
->>>>>>> SideScrollerMap
 }
 
 void Scene::render()
